aee35ba3c509cd5a91810b3b77b98a84d6f100bf
Hikam666 <aklilhikam6@gmail.com>
2025-06-05 14:06:43 +0700

    Class Kendaraan -Hikam

---
+++ kendaraan.java
@@ -0,0 +1,15 @@
+public class kendaraan {
+    String platNomor;
+    String tipe;
+    String merek;
+
+    public kendaraan(String plat, String tipe, String merek) {
+        platNomor = plat;
+        this.tipe = tipe;
+        this.merek = merek;
+    }
+
+    public void tampilInformasi () {
+        System.out.println(platNomor + " - " + tipe + " - " + merek);
+    }
+}
